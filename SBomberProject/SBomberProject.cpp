393c7716b256bdd4774b5540b935da2a6cc10d67
Aleksei Klopkov <kasystems@mail.ru>
2022-02-06 01:03:22 +0300

    Добавьте файлы проекта.

---
+++ SBomberProject/SBomberProject.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <conio.h>
+#include <thread>
+#include <chrono>
+
+#include "SBomber.h"
+#include "MyTools.h"
+
+using namespace std;
+
+//========================================================================================================================
+
+int main(void)
+{
+    MyTools::OpenLogFile("log.txt");
+
+    SBomber game;
+
+    do {
+        game.TimeStart();
+
+        if (_kbhit())
+        {
+            game.ProcessKBHit();
+        }
+        std::this_thread::sleep_for(500ms);
+        MyTools::ClrScr();
+
+        game.DrawFrame();
+        game.MoveObjects();
+        game.CheckObjects();
+
+        game.TimeFinish();
+
+    } while (!game.GetExitFlag());
+
+    MyTools::CloseLogFile();
+
+    return 0;
+}
